e7e4880d673127cbafa2f0dce971ea94fab87fce
jingerlab <jingerlab.in@gmail.com>
2026-06-25 21:56:53 +0530

    second attempt after the fuckup ; Something i tried

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	// Forces all asset assets paths to be built relative to index.html
-	base: './',
+	// Tells Vite your exact repository subdirectory path
+	base: '/2026_daup_lecture_2/',
 	build: {
 		assetsDir: 'assets',
+		// Keeps chunk warning logs quiet for large extensions like Monaco/Shiki
+		chunkSizeWarningLimit: 10000,
 	}
 })
