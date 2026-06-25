453c26094b06812dda5f3216d4ec6880600ebff1
jingerlab <jingerlab.in@gmail.com>
2026-06-26 00:29:51 +0530

    ci: pipeline transition to cloud native GitHub Actions deployment 5

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	// Must have BOTH a leading and a trailing slash!
-	base: '/2026_daup_lecture_2/',
+	base: './',
 	build: {
 		assetsDir: 'assets'
 	}
